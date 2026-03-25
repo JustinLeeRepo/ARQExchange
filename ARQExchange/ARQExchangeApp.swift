38f062bfd837632c8d14783dc07dbaebb2bad660
Justin Lee <justinj.lee23@gmail.com>
2026-03-25 10:55:59 -0700

    Initial Commit

---
+++ ARQExchange/ARQExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARQExchangeApp.swift
+//  ARQExchange
+//
+//  Created by Justin Lee on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct ARQExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARQExchange/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARQExchange
+//
+//  Created by Justin Lee on 3/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
